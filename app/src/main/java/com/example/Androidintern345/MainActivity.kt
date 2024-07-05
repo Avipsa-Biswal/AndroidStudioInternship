f1a21d7ee2c343a0a3a3138fc6f26a00fcf9c48e
Avipsa-Biswal <cse.22bcse37@silicon.ac.in>
2024-07-05 22:03:20 +0530

    assignment for 5thJuly

--- app/src/main/java/com/example/Androidintern345/MainActivity.kt
+++ app/src/main/java/com/example/Androidintern345/MainActivity.kt
@@ -1,38 +1,82 @@
 package com.example.Androidintern345
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.LocalTextStyle
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.semantics.Role.Companion.Switch
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.Androidintern345.ui.theme.MyApplicationTheme
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.PlatformTextStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.Hyphens
-import androidx.compose.ui.text.style.LineBreak
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.em
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,162 +85,360 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Column(
+
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
-                    //Set text alignment
-                    CenterText()
-                    //Add multiple styles in a paragraph
-                    ParagraphStyle1()
-                    //Adjust line height and padding
-                    HeightPadding()
-                    //Insert line breaks
-                    LineBreaks1()
-                    //Hyphenate text split across lines
-                    HyphenateText()
-                    //limit visible lines
-                    LongText()
-                    //Indicate overflow in text
-                    OverflowedText()
+                    // Buttons(All types)
+                    FilledButton()
+                    FilledTonalButtonExample()
+                    OutlinedButtonExample()
+                    RectangleOutlinedButtonExample1()
+                    RectangleOutlinedButtonExample2()
+                    ElevatedButtonExample()
+                    TextButtonExample()
+                    SampleButton()
 
+                    //- Card
+                    FilledCardExample()
+                    ElevatedCardExample()
+                    OutlinedCardExample()
 
-                    }
+                    //- Chip
+                    AssistChipExample()
+                    FilterChipExample()
+                    InputChipExample(
+                        text = "Example Chip",
+                        onDismiss = { /* Handle dismiss */ }
+                    )
+                    //- Slider
+                    SliderMinimalExample()
+                    RangeSliderExample()
+                    //- Switch
+                    SwitchMinimalExample()
+                    SwitchWithIconExample()
+                    SwitchWithCustomColors()
+                    //- Divider
+                    HorizontalDividerExample()
+                    VerticalDividerExample()
+                    //- Progress Indicator
+                    IndeterminateCircularIndicator()
+                }
 
             }
         }
 
     }
-}
 
 
-@Composable
-fun CenterText() {
-    Text("Hello Everyone!", textAlign = TextAlign.Center, modifier = Modifier.width(150.dp)
-    )
 }
 
 @Composable
-fun ParagraphStyle1() {
-    Text(
-        buildAnnotatedString {
-            withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
-                withStyle(style = SpanStyle(color = Color.Blue)) {
-                    append("Hello\n")
-                }
-                withStyle(
-                    style = SpanStyle(
-                        fontWeight = FontWeight.Bold, color = Color.Red
-                    )
-                ) {
-                    append("World\n")
-                }
-                append("Compose")
-            }
-        }
-    )
+fun FilledButton(){
+    Button(onClick = {
+        println("Button: clicked")
+                     },
+        colors = ButtonDefaults.buttonColors(Color.DarkGray)){
+        Text(text = "Filled", color = Color.Red)
+    }
+}
+@Composable
+fun FilledTonalButtonExample() {
+    FilledTonalButton(onClick = { println("Button: clicked") }) {
+        Text("Tonal")
+    }
+}
+@Composable
+fun OutlinedButtonExample() {
+    OutlinedButton(onClick = { println("Button: clicked") }) {
+        Text("Outlined")
+    }
+}
+@Composable
+fun RectangleOutlinedButtonExample1() {
+    OutlinedButton(onClick = { println("Button: clicked")}, shape = RectangleShape) {
+        Text("Rectangle")
+    }
+}
+@Composable
+fun RectangleOutlinedButtonExample2() {
+    OutlinedButton(onClick = { println("Button: clicked")}, shape = RoundedCornerShape(5.dp)) {
+        Text("Rectangle")
+    }
+}
+@Composable
+fun ElevatedButtonExample() {
+    ElevatedButton(onClick = { println("Button: clicked") }) {
+        Text("Elevated")
+    }
 }
 
 @Composable
-fun HeightPadding() {
-    Text(
-        text = "Hello world!\nIts a wonderful\n day",
-        style = LocalTextStyle.current.merge(
-            TextStyle(
-                lineHeight = 2.5.em,
-                platformStyle = PlatformTextStyle(
-                    includeFontPadding = false
-                ),
-                lineHeightStyle = LineHeightStyle(
-                    alignment = LineHeightStyle.Alignment.Center,
-                    trim = LineHeightStyle.Trim.FirstLineTop
-                )
+fun TextButtonExample() {
+    TextButton(onClick = { println("Button: clicked") }) {
+        Text("Text")
+    }
+}
+
+@Composable
+fun SampleButton(){
+    Button(onClick = { println("Button: Clicked")},
+        shape = RoundedCornerShape(3.dp),
+        colors = ButtonDefaults.buttonColors(Color.Green)
+    ){
+        Icon(
+            Icons.Rounded.ShoppingCart
+            ,contentDescription ="New" )
+        Text(text = " Add to Cart")
+    }
+}
+
+@Composable
+fun FilledCardExample() {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text(
+            text = "Filled",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun ElevatedCardExample() {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text(
+            text = "Elevated",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun OutlinedCardExample() {
+    OutlinedCard(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, Color.Black),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text(
+            text = "Outlined",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+@Composable
+fun AssistChipExample() {
+    AssistChip(
+        onClick = { Log.d("Assist chip", "hello world") },
+        label = { Text("Assist chip") },
+        leadingIcon = {
+            Icon(
+                Icons.Filled.Settings,
+                contentDescription = "Localized description",
+                Modifier.size(AssistChipDefaults.IconSize)
             )
-        )
+        }
+    )
+}
+@Composable
+fun FilterChipExample() {
+    var selected by remember { mutableStateOf(false) }
+
+    FilterChip(
+        onClick = { selected = !selected },
+        label = {
+            Text("Filter chip")
+        },
+        selected = selected,
+        leadingIcon = if (selected) {
+            {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "Done icon",
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )
+            }
+        } else {
+            null
+        },
+    )
+}
+@Composable
+fun InputChipExample(
+    text: String,
+    onDismiss: () -> Unit,
+) {
+    var enabled by remember { mutableStateOf(true) }
+    if (!enabled) return
+
+    InputChip(
+        onClick = {
+            onDismiss()
+            enabled = !enabled
+        },
+        label = { Text(text) },
+        selected = enabled,
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Default.Close,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
     )
 }
 
+
 @Composable
-fun TextSample(samples: Map<String, @Composable () -> Unit>) {
+fun SliderMinimalExample() {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
     Column {
-        samples.forEach { (label, content) ->
-            Text(text = label, fontSize = 16.sp, modifier = Modifier.padding(8.dp))
-            content()
-        }
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it }
+        )
+        Text(text = sliderPosition.toString())
     }
 }
 
 @Composable
-fun LineBreaks1() {
-    TextSample(
-        samples = mapOf(
-            "Simple" to {
-                Text(
-                    text = "Jetpack Compose is Android's modern toolkit for building native Ul. It simpli- fies and accelerates Ul development on Android bringing your apps to life with less code, powerful tools, and intuitive Kotlin APIs. ",
-                    modifier = Modifier
-                        .width(180.dp)
-                        .border(BorderStroke(1.dp, Color.Gray))
-                        .padding(8.dp),
-                    fontSize = 14.sp,
-                    style = TextStyle.Default
-                )
+fun RangeSliderExample() {
+    var sliderPosition by remember { mutableStateOf(0f..100f) }
+    Column {
+        RangeSlider(
+            value = sliderPosition,
+            steps = 5,
+            onValueChange = { range -> sliderPosition = range },
+            valueRange = 0f..100f,
+            onValueChangeFinished = {
+                // launch some business logic update with the state you hold
+                // viewModel.updateSelectedSliderValue(sliderPosition)
             },
-            "Paragraph" to {
-                Text(
-                    text = "Jetpack Compose is Android's modern toolkit for building native Ul. It simpli- fies and accelerates Ul development on Android bringing your apps to life with less code, powerful tools, and intuitive Kotlin APIs. ",
-                    modifier = Modifier
-                        .width(180.dp)
-                        .border(BorderStroke(1.dp, Color.Gray))
-                        .padding(8.dp),
-                    fontSize = 14.sp,
-                    style = TextStyle.Default
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
+
+@Composable
+fun SwitchMinimalExample() {
+    var checked by remember { mutableStateOf(true) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+        }
+    )
+}
+@Composable
+fun SwitchWithIconExample() {
+    var checked by remember { mutableStateOf(true) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+        },
+        thumbContent = if (checked) {
+            {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
                 )
             }
+        } else {
+            null
+        }
+    )
+}
+@Composable
+fun SwitchWithCustomColors() {
+    var checked by remember { mutableStateOf(true) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+        },
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = MaterialTheme.colorScheme.primary,
+            checkedTrackColor = MaterialTheme.colorScheme.primaryContainer,
+            uncheckedThumbColor = MaterialTheme.colorScheme.secondary,
+            uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
         )
     )
 }
 
 @Composable
-fun HyphenateText() {
-    TextSample(
-        samples = mapOf(
-            "Hyphens - None" to {
-                Text(
-                    text = "Jetpack Compose is Android's modern toolkit for building native Ul. It simpli- fies and accelerates Ul development on Android bringing your apps to life with less code, powerful tools, and intuitive Kotlin APIs. It makes building Android UI faster and easier.",
-                    modifier = Modifier
-                        .width(200.dp)
-                        .border(BorderStroke(1.dp, Color.Gray)),
-                    fontSize = 14.sp,
-                    style = TextStyle.Default.copy(
-                        lineBreak = LineBreak.Paragraph,
-                        hyphens = Hyphens.None
-                    )
-                )
-            },
-            "Hyphens - Auto" to {
-                Text(
-                    text = "Jetpack Compose is Android's modern toolkit for building native Ul. It simpli- fies and accelerates Ul development on Android bringing your apps to life with less code, powerful tools, and intuitive Kotlin APIs. It makes building Android UI faster and easier.",
-                    modifier = Modifier
-                        .width(200.dp)
-                        .border(BorderStroke(1.dp, Color.Gray)),
-                    fontSize = 14.sp,
-                    style = TextStyle.Default.copy(
-                        lineBreak = LineBreak.Paragraph,
-                        hyphens = Hyphens.Auto
-                    )
-                )
-            }
-        )
+fun HorizontalDividerExample() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Text("First item in list")
+        HorizontalDivider(thickness = 2.dp)
+        Text("Second item in list")
+    }
+}
+
+@Composable
+fun VerticalDividerExample() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text("First item in row")
+        VerticalDivider(color = MaterialTheme.colorScheme.secondary)
+        Text("Second item in row")
+    }
+}
+
+@Composable
+fun IndeterminateCircularIndicator() {
+    var loading by remember { mutableStateOf(false) }
+
+    Button(onClick = { loading = true }, enabled = !loading) {
+        Text("Start loading")
+    }
+
+    if (!loading) return
+
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
     )
-}
-
-@Composable
-fun LongText() {
-    Text("hello ".repeat(50), maxLines = 2)
-}
-
-@Composable
-fun OverflowedText() {
-    Text("Hello Compose ".repeat(50), maxLines = 2, overflow = TextOverflow.Ellipsis)
 }

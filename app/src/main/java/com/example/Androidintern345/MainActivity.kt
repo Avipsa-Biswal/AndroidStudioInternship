30e5c3e366050262470d63ef72e552dceb241de7
Avipsa-Biswal <cse.22bcse37@silicon.ac.in>
2024-07-07 00:38:31 +0530

    assignment for 5thJuly

--- app/src/main/java/com/example/Androidintern345/MainActivity.kt
+++ app/src/main/java/com/example/Androidintern345/MainActivity.kt
@@ -1,9 +1,9 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.Androidintern345
 
 import android.annotation.SuppressLint
+import android.graphics.Color.parseColor
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -14,19 +14,27 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
@@ -43,6 +51,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,11 +59,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.example.Androidintern345.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.launch
@@ -67,143 +81,58 @@ class MainActivity : ComponentActivity() {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(16.dp),
+                        .padding(10.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
                 ) {
-                    //- Scaffold
-                    ScaffoldExample()
-                    //- Bottomsheets
-                    PartialBottomSheet()
-                    BottomsheetExample()
-                    //- Dialogs
-                    DialogExamples()
-                    ShowDialogWithImageExample()
-                    //- TextField
-                    SimpleFilledTextFieldSample()
-                    SimpleOutlinedTextFieldSample()
-                    StyledTextField()
-
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun ScaffoldExample() {
-    var presses by remember { mutableIntStateOf(0) }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text("Top app bar")
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary,
-            ) {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = "Bottom app bar",
-                )
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = """
-                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
-
-                    It also contains some basic inner content, such as this text.
-
-                    You have pressed the floating action button $presses times.
-                """.trimIndent(),
-            )
-        }
-    }
-}
-
-@Composable
-fun PartialBottomSheet() {
-    var showBottomSheet by remember { mutableStateOf(false) }
-    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Button(onClick = { showBottomSheet = true }) {
-            Text("Display partial bottom sheet")
-        }
-
-        if (showBottomSheet) {
-            ModalBottomSheet(
-                modifier = Modifier.fillMaxHeight(),
-                sheetState = sheetState,
-                onDismissRequest = { showBottomSheet = false }
-            ) {
-                Text(
-                    "Swipe up to open sheet. Swipe down to dismiss.",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-        }
-    }
-}
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun BottomsheetExample(){
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(false) }
-    Scaffold(
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Show bottom sheet") },
-                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                onClick = {
-                    showBottomSheet = true
-                }
-            )
-        }
-    ) { contentPadding ->
-        // Screen content
-
-        if (showBottomSheet) {
-            ModalBottomSheet(
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState
-            ) {
-                // Sheet content
-                Button(onClick = {
-                    scope.launch { sheetState.hide() }.invokeOnCompletion {
-                        if (!sheetState.isVisible) {
-                            showBottomSheet = false
+                    ElevatedCard(
+                        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .heightIn(max = 600.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(20.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            AppHeading()
+                            AppImage()
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.Start
+                            ) {
+                                LoginText()
+                            }
+                            EmailOrMobileNoTextField()
+                            PasswordTextField()
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                ForgotPasswordTextButton()
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.Start
+                            ) {
+                                LoginButton()
+                            }
                         }
                     }
-                }) {
-                    Text("Hide bottom sheet")
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        DontHaveAnAccountText()
+                        RegisterTextButton()
+                    }
                 }
             }
         }
@@ -211,183 +140,108 @@ fun BottomsheetExample(){
 }
 
 @Composable
-fun AlertDialogExample(
-    onDismissRequest: () -> Unit,
-    onConfirmation: () -> Unit,
-    dialogTitle: String,
-    dialogText: String,
-    icon: ImageVector,
-) {
-    AlertDialog(
-        icon = {
-            Icon(icon, contentDescription = "Example Icon")
-        },
-        title = {
-            Text(text = dialogTitle)
-        },
-        text = {
-            Text(text = dialogText)
-        },
-        onDismissRequest = {
-            onDismissRequest()
-        },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onConfirmation()
-                }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    onDismissRequest()
-                }
-            ) {
-                Text("Dismiss")
-            }
-        }
+fun AppHeading(){
+    Text(
+        text = "Jetpack Compose",
+        color = "#1c5982".color,
+        fontSize = 28.sp,
     )
 }
 
 @Composable
-fun DialogExamples() {
-    val openAlertDialog = remember { mutableStateOf(false) }
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Button(onClick = { openAlertDialog.value = true }) {
-            Text("Show Alert Dialog")
-        }
-
-        if (openAlertDialog.value) {
-            AlertDialogExample(
-                onDismissRequest = { openAlertDialog.value = false },
-                onConfirmation = {
-                    openAlertDialog.value = false
-                    println("Confirmation registered") // Add logic here to handle confirmation.
-                },
-                dialogTitle = "Alert dialog example",
-                dialogText = "This is an example of an alert dialog with buttons.",
-                icon = Icons.Default.Info
-            )
-        }
-    }
-}
-
-@Composable
-fun SimpleFilledTextFieldSample() {
-    var text by remember { mutableStateOf("Hello") }
-
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Label") }
+fun AppImage(){
+    Image(
+        modifier = Modifier
+            .size(150.dp),
+        painter = painterResource(id = R.drawable.compose_logo),
+        contentDescription = "This is our app logo"
     )
 }
+
 @Composable
-fun SimpleOutlinedTextFieldSample() {
+fun LoginText() {
+    Text(
+        text = "Login",
+        color = "#0d6837".color,
+        fontSize = 30.sp
+    )
+}
+
+@Composable
+fun EmailOrMobileNoTextField() {
     var text by remember { mutableStateOf("") }
 
     OutlinedTextField(
         value = text,
         onValueChange = { text = it },
-        label = { Text("Label") }
+        label = { Text("Email ID or Mobile Number") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
     )
 }
 
 @Composable
-fun StyledTextField() {
-    var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
+fun PasswordTextField() {
+    var password by rememberSaveable { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
-    TextField(
-        value = value,
-        onValueChange = { value = it },
-        label = { Text("Enter text") },
-        maxLines = 2,
-        textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
-        modifier = Modifier.padding(20.dp)
-    )
-}
-
-@Composable
-fun ShowDialogWithImageExample() {
-    var showDialog by remember { mutableStateOf(false) }
-
-    val painter = painterResource(id = R.drawable.sample_1)
-    val imageDescription = "Sample Image"
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Button(onClick = { showDialog = true }) {
-            Text("Show Dialog With Image")
-        }
-
-        if (showDialog) {
-            DialogWithImage(
-                onDismissRequest = { showDialog = false },
-                onConfirmation = { /* Handle confirmation action */ showDialog = false },
-                painter = painter,
-                imageDescription = imageDescription,
-            )
-        }
-    }
-}
-
-@Composable
-fun DialogWithImage(
-    onDismissRequest: () -> Unit,
-    onConfirmation: () -> Unit,
-    painter: Painter,
-    imageDescription: String,
-) {
-    Dialog(onDismissRequest = { onDismissRequest() }) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(375.dp)
-                .padding(16.dp),
-            shape = RoundedCornerShape(16.dp),
-        ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Enter password") },
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            IconButton(
+                onClick = { isPasswordVisible = !isPasswordVisible }
             ) {
-                Image(
-                    painter = painter,
-                    contentDescription = imageDescription,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.height(160.dp)
+                Icon(
+                    painter = painterResource(id = R.drawable.show_password_icon),
+                    contentDescription = "Toggle password visibility"
                 )
-                Text(
-                    text = "This is a dialog with buttons and an image.",
-                    modifier = Modifier.padding(16.dp),
-                )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    TextButton(
-                        onClick = { onDismissRequest() },
-                        modifier = Modifier.padding(8.dp),
-                    ) {
-                        Text("Dismiss")
-                    }
-                    TextButton(
-                        onClick = { onConfirmation() },
-                        modifier = Modifier.padding(8.dp),
-                    ) {
-                        Text("Confirm")
-                    }
-                }
             }
-        }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    )
+}
+
+@Composable
+fun ForgotPasswordTextButton(){
+    TextButton(onClick = { println("Button: clicked") }) {
+        Text(
+            text = "Forgot Password?",
+            color = "#0d6837".color
+        )
     }
 }
+
+@Composable
+fun LoginButton(){
+    Button(onClick = {
+        println("Button: clicked")
+    },
+        colors = ButtonDefaults.buttonColors("#1c5982".color)){
+        Text(text = "Login")
+    }
+}
+@Composable
+fun DontHaveAnAccountText()
+ {
+    Text(
+        text = "Don't have an account?"
+    )
+}
+@Composable
+fun RegisterTextButton(){
+    TextButton(onClick = { println("Button: clicked") }) {
+        Text(
+            text = "Register",
+            color = "#1c5982".color
+        )
+    }
+}
+val String.color
+    get() = Color(parseColor(this))

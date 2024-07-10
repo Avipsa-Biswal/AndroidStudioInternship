d31c41faf024c1a09aa393944939da92325953c1
Avipsa-Biswal <cse.22bcse37@silicon.ac.in>
2024-07-10 21:54:56 +0530

    assignment for 10thJuly

--- app/src/main/java/com/example/Androidintern345/MainActivity.kt
+++ app/src/main/java/com/example/Androidintern345/MainActivity.kt
@@ -1,139 +1,149 @@
+//Make Sure you add dependencies in build.gradle file for Retrofit and Gson
+// implementation(libs.retrofit)
+// implementation(libs.converter.gson)
+// Add Permission for Internet in Android Manifest file
+//<uses-permission android:name="android.permission.INTERNET"/>
+
 package com.example.Androidintern345
 
-import android.graphics.Color.parseColor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalProvider
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontStyle.*
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.lifecycle.viewModelScope
 import com.example.Androidintern345.ui.theme.MyApplicationTheme
-
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(20.dp)
-            ) {
-                //Function to implement basic MVVM architecture
-                CounterView()
-            }
-        }
-    }
-
-    @Composable
-    fun CounterView(counterVM: CounterViewModel = viewModel()) {
-        val counterState = counterVM.counter.value
-        var intContent = GenericClass(129)
-        var stringContent = GenericClass("hello generics")
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(text = "Current Counter Value: ${counterState.count}")
-            Row {
-                Button(onClick = { counterVM.incrementCounter() }) {
-                    Text(text = "Increment")
-                }
-            Spacer(modifier = Modifier.width(20.dp))
-            Button(onClick = { counterVM.decrementCounter() }) {
-                Text(text = "decrement")
-                }
-                Spacer(modifier = Modifier.width(20.dp))
-                Button(onClick = { counterVM.login()
-                println("${intContent.content},${stringContent.content}")})
-                {
-                    Text(text = "login")
+            var value by remember { mutableFloatStateOf(0f) }
+            var checked by remember { mutableStateOf(true) }
+            val productVM : ProductViewModel by viewModels()
+            MyApplicationTheme {
+                Column(
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = {
+                        productVM.fetchProducts()
+                    }) {
+                        Text(text = "Click to make an API Call")
+                    }
                 }
             }
         }
     }
 }
+
 
 //Model Class
-data class Counter(val count: Int)
-data class User(val username: String, val password: String)
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
+    val rating: Rating
+)
 
-//ViewModelClass
-class CounterViewModel : ViewModel() {
-    private val _counter = mutableStateOf(Counter(0))
-    val counter: State<Counter> = _counter
-
-    fun incrementCounter() {
-        _counter.value = Counter(_counter.value.count + 1)
-    }
-    fun decrementCounter() {
-        _counter.value = Counter(_counter.value.count - 1)
-    }
-    fun resetCounter() {
-        _counter.value = Counter(0)
-    }
-    fun login() {
-        //login
-    }
-
+data class  Rating (
+    val rate: Double,
+    val count: Int
+)
+interface  ApiService {
+    @GET("products")
+    suspend fun getProducts(): List<Product>
 }
+object RetrofitClient {
+    private const val BASE_URL = "https://fakestoreapi.com/"
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
+class ProductRepository(private val apiService: ApiService) {
+    suspend fun getProducts(): List<Product> {
+        return  apiService.getProducts()
+    }
+}
+class ProductViewModel: ViewModel() {
+    private val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> get() = _products
 
-class GenericClass<T>(var content: T)
+    private val repository = ProductRepository(RetrofitClient.apiService)
+
+    fun fetchProducts() {
+        viewModelScope.launch {
+            try {
+                val productList = repository.getProducts()
+                _products.postValue(productList)
+                println("API DATA CALLED: $productList")
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+}

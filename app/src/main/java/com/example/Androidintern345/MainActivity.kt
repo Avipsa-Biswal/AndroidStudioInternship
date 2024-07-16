0f17e7624e24cde9b8f240e87bfd147dc221c3b7
Avipsa-Biswal <cse.22bcse37@silicon.ac.in>
2024-07-16 23:01:52 +0530

    assignment for 16thJuly

--- app/src/main/java/com/example/Androidintern345/MainActivity.kt
+++ app/src/main/java/com/example/Androidintern345/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.Androidintern345
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -36,6 +38,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.OnTrimMemoryProvider
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -57,33 +60,106 @@ import com.example.Androidintern345.ui.theme.MyApplicationTheme
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
 import com.google.firebase.FirebaseApp
-
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
-    private val auth: FirebaseAuth by lazy {
-        FirebaseAuth.getInstance()
-    }
+
+    private val TAG = "MainActivity"
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private var storedVerificationId: String? = null
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this) // Ensure Firebase is initialized
+        FirebaseApp.initializeApp(this)
         setContent {
             MyApplicationTheme {
-                val navController = rememberNavController()
-                NavHost(navController, startDestination = "login") {
-                    composable("login") { LoginScreen(navController) }
-                    composable("register") { RegisterScreen(navController) }
-                    composable("home") { HomeScreen() }
-                }
+                LoginScreen()
             }
         }
     }
 
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            Log.d(TAG, "onVerificationCompleted:$credential")
+            // signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            Log.w(TAG, "onVerificationFailed", e)
+            when (e) {
+                is FirebaseAuthInvalidCredentialsException -> {
+                    // Invalid request
+                }
+                is FirebaseTooManyRequestsException -> {
+                    // SMS quota exceeded
+                }
+                is FirebaseAuthMissingActivityForRecaptchaException -> {
+                    // reCAPTCHA verification attempted with null Activity
+                }
+                else -> {
+                    // Handle other errors
+                }
+            }
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            Log.d(TAG, "onCodeSent:$verificationId")
+            storedVerificationId = verificationId
+            resendToken = token
+        }
+    }
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun verifyOTP(otp: String) {
+        storedVerificationId?.let {
+            val credential = PhoneAuthProvider.getCredential(it, otp)
+            signInWithPhoneAuthCredential(credential)
+        }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = task.result?.user
+                    // Update UI with user info
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // Invalid verification code
+                    }
+                    // Update UI
+                }
+            }
+    }
+
     @Composable
-    fun LoginScreen(navController: NavHostController) {
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var errorMessage by remember { mutableStateOf<String?>(null) }
+    fun LoginScreen() {
+        var phoneNumber by remember { mutableStateOf("") }
+        var otp by remember { mutableStateOf("") }
 
         Scaffold {
             Column(
@@ -101,135 +177,40 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column(modifier = Modifier.padding(16.dp)) {
                         TextField(
-                            value = email,
-                            onValueChange = { email = it },
-                            label = { Text(text = "Email") },
+                            value = phoneNumber,
+                            onValueChange = { phoneNumber = it },
+                            label = { Text(text = "Enter phone number") },
                             modifier = Modifier
                                 .fillMaxWidth()
-                                 )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = password,
-                            onValueChange = { password = it },
-                            label = { Text(text = "Password") },
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            visualTransformation = PasswordVisualTransformation(),
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                                .clip(CircleShape)
                         )
                         Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick = { signIn(email, password, navController, setError = { errorMessage = it }) }) {
-                            Text(text = "Login")
+                        Button(onClick = { startPhoneNumberVerification(phoneNumber) }) {
+                            Text(text = "Send OTP")
                         }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        TextButton(onClick = { navController.navigate("register") }) {
-                            Text(text = "Register")
-                        }
-                        errorMessage?.let {
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Text(text = it)
+
+                        OutlinedTextField(
+                            value = otp,
+                            onValueChange = { otp = it },
+                            label = { Text(text = "Enter OTP") },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(CircleShape)
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = { verifyOTP(otp) }) {
+                            Text(text = "Validate OTP")
                         }
                     }
                 }
             }
         }
-    }
-
-    @Composable
-    fun RegisterScreen(navController: NavHostController) {
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var errorMessage by remember { mutableStateOf<String?>(null) }
-
-        Scaffold {
-            Column(
-                modifier = Modifier
-                    .padding(it)
-                    .padding(50.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        TextField(
-                            value = email,
-                            onValueChange = { email = it },
-                            label = { Text(text = "Email") },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = password,
-                            onValueChange = { password = it },
-                            label = { Text(text = "Password") },
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            visualTransformation = PasswordVisualTransformation(),
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick = { signUp(email, password, navController, setError = { errorMessage = it }) }) {
-                            Text(text = "Register")
-                        }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        TextButton(onClick = { navController.navigate("login") }) {
-                            Text(text = "Login")
-                        }
-                        errorMessage?.let {
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Text(text = it)
-                        }
-                    }
-                }
-            }
-        }
-    }
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @Composable
-    fun HomeScreen() {
-        Scaffold {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Welcome to the Home Screen!", fontSize = 24.sp)
-            }
-        }
-    }
-
-
-    private fun signIn(email: String, password: String, navController: NavHostController, setError: (String?) -> Unit) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnSuccessListener {
-                navController.navigate("home") {
-                    popUpTo("login") { inclusive = true }
-                }
-            }
-            .addOnFailureListener {
-                setError(it.message)
-            }
-    }
-
-    private fun signUp(email: String, password: String, navController: NavHostController, setError: (String?) -> Unit) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnSuccessListener {
-                navController.navigate("home") {
-                    popUpTo("register") { inclusive = true }
-                }
-            }
-            .addOnFailureListener {
-                setError(it.message)
-            }
     }
 }
+
+
+
 
 
 
